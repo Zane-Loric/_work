343becb8d9deac65ea2fab2c2d365aafec2fd634
Rohan Paudyal <rohanpaudyal15@gmail.com>
2025-06-26 16:47:52 +0545

    Sequence

---
+++ 2_0).py
@@ -0,0 +1,16 @@
+#python program to generate a pyramid structure asking input from user
+def Seq(User_input):
+    for i in range(1,User_input+1):
+        print(" "*(User_input-i),end="")
+        print("*"*(2*i-1), end="")
+        print()
+        
+
+
+
+
+
+
+
+User_input = int(input("Enter a number: "))
+Seq(User_input)
