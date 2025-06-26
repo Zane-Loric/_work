88777c73be6e4eed4ac7689feda2e570811a127a
Zane Loric <rohanpaudyal15@gmail.com>
2025-06-26 19:30:16 +0545

    Update

--- 3_0).py
+++ 3_0).py
@@ -1,8 +1,8 @@
-def s(a):
+def seq(a):
     for i in range(1,a+1):
         print("*"*(a-i),end="") #Number of star in a single row
         print(" ")
 
 a = int(input("Enter the number of rows: "))
-s(a)
+seq(a)
 #By Zane Loric
