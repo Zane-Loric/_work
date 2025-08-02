5be3056b2daa0efd04a47b3074d66dfda584326d
Zane Loric <rohanpaudyal15@gmail.com>
2025-08-02 07:20:33 +0545

    Null__

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
 import pyttsx3
 engine = pyttsx3.init ()
-engine.say("Happy! Fucking! New Year! Kuma! Sagar")
+engine.say("Happy! New Year! Kuma! Sagar")
 engine. runAndWait ()       #Voice Over project
